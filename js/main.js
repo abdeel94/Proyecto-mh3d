209bd0ebb215ac4e92fc61ebe48fce624ff8fa42
jimegonzalez <jim.gonzalez@duocuc.cl>
2022-03-31 09:42:58 -0300

    añadida carpeta java script

--- js/main.js
+++ js/main.js
@@ -1,13 +1,17 @@
-document.addEventListener('DOMContentLoaded', () => {
-    const elementosCarousel = document.querySelectorAll('.carousel');
-    M.Carousel.init(elementosCarousel, {
-        duration: 20,
-        dist: -80,
-        shift: 5,
-        padding: 5,
-        numVisible: 4,
-        indicators: true,
-        noWrap: false
-    });
-
-});
+(function() {
+    'use strict';
+    window.addEventListener('load', function() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        var forms = document.getElementsByClassName('needs-validation');
+        // Loop over them and prevent submission
+        var validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            }, false);
+        });
+    }, false);
+})();
